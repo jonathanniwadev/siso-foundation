47605ea5899afe7f9a4222ba2bf4c838a65de81e
Jonathan Niwagaba <jonnyniwa@gmail.com>
2026-05-28 18:42:28 +0300

    Comment out HomeLatestUpdates in page.tsx
    Comment out HomeLatestUpdates component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -118,7 +118,7 @@ export default function HomePage() {
       </section>
 
       <HomePartnersSection />
-      <HomeLatestUpdates />
+      {/* <HomeLatestUpdates /> */}
       <HomeBoardSection />
       <HomeDonateSection />
 
